879bf9b5a91053498efd7eb9ef9494dcd0b23287
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-05-03 18:06:17 -0300

    aula_Uninter de java/springboot

---
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -0,0 +1,100 @@
+package br.com.springboot.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+	
+	// Atributos
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 50)
+	private String nome;
+	@Column(length = 14)
+	private String cpf;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@Column(name= "data_nascimento", columnDefinition = "DATE")
+	private LocalDate dataDeNascimento;
+	@Enumerated(EnumType.STRING)
+	private Sexo sexo;
+	@Column(length = 14)
+	private String telefone;
+	@Column(length = 14)
+	private String celular;
+	@Column(length = 50)
+	private String email;
+	private boolean ativo;
+
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public Sexo getSexo() {
+		return sexo;
+	}
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+
+}
---
+++ src/test/java/br/com/springboot/bo/ClienteBOTest.java
@@ -0,0 +1,54 @@
+package br.com.springboot.bo;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import br.com.springboot.model.Cliente;
+import br.com.springboot.model.Sexo;
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(OrderAnnotation.class)
+
+public class ClienteBOTest {
+	@Autowired
+	private ClienteBO bo;
+	
+	@Test
+	@Order(1)
+	public void insere() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("José da Silva");
+		cliente.setCpf("12345678900");
+		cliente.setDataDeNascimento(LocalDate.of(2000, 1, 8));
+		cliente.setSexo(Sexo.MASCULINO);
+		cliente.setTelefone("4133333333");
+		cliente.setCelular("41999999999");
+		cliente.setAtivo(true);
+		cliente.setEmail("sivaldo@gmail.com");
+		bo.insere(cliente);
+		}
+	
+	@Test
+	@Order(2)
+	public void pesquisaPeloId() {
+		Cliente cliente = bo.pesquisaPeloId(1L);
+		System.out.println(cliente);
+		}
+	@Test
+	@Order(3)
+	public void atualiza() {
+		Cliente cliente = bo.pesquisaPeloId(1L);
+		cliente.setCpf("98765432100");
+		bo.atualiza(cliente);
+		}
+	
+	
+
+}
