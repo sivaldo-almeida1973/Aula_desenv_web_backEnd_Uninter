7113d3ccc0e04cead9300fde940e331f1d210b7e
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-05-13 11:38:40 -0300

    criado os metodos edita e remove ,na classe NotaEntradaItemController

--- src/main/java/br/com/springboot/controller/NotaEntradaItemController.java
+++ src/main/java/br/com/springboot/controller/NotaEntradaItemController.java
@@ -1,12 +1,15 @@
 package br.com.springboot.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.springboot.bo.NotaEntradaBO;
@@ -42,7 +45,11 @@ public class NotaEntradaItemController {
 		if ( produtoId == null) {
 			result.rejectValue("produto", "field.required");
 		}
-		//validacao do produto
+		
+		//adicionar validacao
+		if (notaEntradaItemBO.itemJaAdicionado(notaEntradaItem)) {
+			result.rejectValue("produto", "duplicate");
+		}
 		
 		
 		//validacao duplicidade
@@ -66,6 +73,23 @@ public class NotaEntradaItemController {
 		 Long notaEntradaId = notaEntradaItem.getNotaEntrada().getId();
 		 return "redirect:/nota-entrada-edita/" + notaEntradaId;
 		
+	}
+	
+	@RequestMapping(value="/edita/{id}", method=RequestMethod.GET)
+	public ModelAndView edita(@PathVariable("id") long id, ModelMap model) {
+		model.addAttribute("notaEntradaItem", notaEntradaItemBO.pesquisaPeloId(id));
+		model.addAttribute("produtos", produtoBO.lista());
+		return new ModelAndView("/nota-entrada-item/formulario", model);
+	}
+	
+	@RequestMapping(value="/remove/{id}", method=RequestMethod.GET)
+    public String remove(@PathVariable("id") long id, RedirectAttributes attr) {
+    	long notaId = 0L;
+    	NotaEntradaItem notaEntradaItem = notaEntradaItemBO.pesquisaPeloId(id);
+    	notaId = notaEntradaItem.getNotaEntrada().getId();
+    	 notaEntradaItemBO.remove(notaEntradaItem);
+    	 attr.addAttribute("feedback", "Item removido com sucesso!");
+    	 return "redirect:/nota-entrada/edita/" + notaId;
 	}
 
 }
